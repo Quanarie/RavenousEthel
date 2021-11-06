c24de1e1ecb46c208f78776f4d4ca7bf65b42c73
sppeeedd <qustaine@gmail.com>
2021-11-06 20:37:39 +0200

    Added enemyMovement

---
+++ Assets/Scripts/Enemy/EnemyMeleeMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMeleeMovement : Movement
+{
+    [SerializeField] private float chasingDistance;
+
+    private bool isAttacking;
+
+    private void Update()
+    {
+        if (isAttacking)
+            return;
+
+        Vector3 playerPos = GameManager.Instance.Player.position;
+
+        if (Vector3.Distance(transform.position, playerPos) <= chasingDistance)
+        {
+            UpdateMotor(new Vector3(playerPos.x - transform.position.x, playerPos.y - transform.position.y, 0).normalized);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out PlayerHealth _))
+            isAttacking = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out PlayerHealth _))
+            isAttacking = false;
+    }
+}
