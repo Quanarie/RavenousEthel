bffa398a1d733a4b9f689e9a407dc32abb99cdd0
sppeeedd <qustaine@gmail.com>
2021-11-28 22:18:24 +0200

    2 enemies animated

--- Assets/Scripts/Other/AdsManager.cs
+++ Assets/Scripts/Other/AdsManager.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using UnityEngine.Advertisements;
 using UnityEngine;
 
-public class AdsManager : MonoBehaviour
+public class AdsManager : MonoBehaviour, IUnityAdsListener
 {
 
     private string iosVideo = "Interstitial_iOS";
+    private string iosRewardedVideo = "Rewarded_iOS";
 
 #if UNITY_IOS
     string gameId = "4477704";
@@ -17,6 +18,7 @@ public class AdsManager : MonoBehaviour
     private void Start()
     {
         Advertisement.Initialize(gameId);
+        Advertisement.AddListener(this);
     }
 
     public void PlayAd()
@@ -24,6 +26,37 @@ public class AdsManager : MonoBehaviour
         if (Advertisement.IsReady(iosVideo))
         {
             Advertisement.Show(iosVideo);
+        }
+    }
+
+    public void PlayRewardedAd()
+    {
+        if (Advertisement.IsReady(iosRewardedVideo))
+        {
+            Advertisement.Show(iosRewardedVideo);
+        }
+    }
+
+    public void OnUnityAdsReady(string placementId)
+    {
+        
+    }
+
+    public void OnUnityAdsDidError(string message)
+    {
+        
+    }
+
+    public void OnUnityAdsDidStart(string placementId)
+    {
+        
+    }
+
+    public void OnUnityAdsDidFinish(string placementId, ShowResult showResult)
+    {
+        if (placementId == iosRewardedVideo && showResult == ShowResult.Finished)
+        {
+
         }
     }
 }
--- Assets/Scripts/Other/GameManager.cs
+++ Assets/Scripts/Other/GameManager.cs
@@ -12,7 +12,6 @@ public class GameManager : MonoBehaviour
     public Transform WeaponParent;
     public WeaponManager weaponManager;
     public Slider weaponStock;
-    public AudioManager audioManager;
 
     public GameObject pickupableArrowObject;
 
